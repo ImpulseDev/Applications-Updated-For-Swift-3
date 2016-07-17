a6f14f8e2444b79a7cb305cae7baf601ba48e33a
ImpulseDev <dev@impulsedesigns.net>
2016-07-17 22:14:59 +1000

    Xcode Project - Auto Messages
    Auto Messages Applications

---
+++ AutoMessage/AutoMessage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoMessage
+//
+//  Created by Josh on 17/07/2016.
+//  Copyright © 2016 Impulse Designs & Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
